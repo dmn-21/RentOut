56bd159aa8a53d86fd8987fa75f0e44fecf64206
Daniel Nikolov <126501@students.ue-varna.bg>
2024-03-19 19:13:01 +0200

    Agent Service added

---
+++ RentOut.Core/Contracts/IRentierService.cs
@@ -0,0 +1,15 @@
+﻿namespace RentOut.Core.Contracts
+{
+    public interface IRentierService
+    {
+        Task<bool> ExistByIdAsync(string userId);
+
+        Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber);
+
+        Task<bool> UserHasRentsAsync(string userId);
+
+        Task CreateAsync(string userId, string phoneNumber);
+
+        Task<int?> GetRentierIdAsync(string userId);
+    }
+}
