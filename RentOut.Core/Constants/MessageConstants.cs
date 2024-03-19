605dc845f68f2abedff14de9f327ce53bd387cde
Daniel Nikolov <126501@students.ue-varna.bg>
2024-03-19 19:26:42 +0200

    Become an agent implemented

---
+++ RentOut.Core/Constants/MessageConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace RentOut.Core.Constants
+{
+    public class MessageConstants
+    {
+        public const string RequiredMessage = "The {0} field is required";
+
+        public const string LengthMessage = "The field {0} must be between {2} and {1} characters long";
+
+        public const string PhoneExists = "Phone Number already exists. Enter another one";
+
+        public const string HasRents = "You should have no rents to become a rentier";
+    }
+}
--- RentOut/Controllers/RentierController.cs
+++ RentOut/Controllers/RentierController.cs
@@ -1,20 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentOut.Attributes;
+using RentOut.Core.Contracts;
 using RentOut.Core.Models.Rentier;
+using System.Security.Claims;
+using static RentOut.Core.Constants.MessageConstants;
 
 namespace RentOut.Controllers
 {
-    public class RentierController : Controller
+    public class RentierController : BaseController
     {
+        private readonly IRentierService rentierService;
+
+        public RentierController(IRentierService _rentierService)
+        {
+            rentierService = _rentierService;
+        }
+
         [HttpGet]
+        [NotAnRentier]
         public IActionResult Become()
         {
             var model = new BecomeRentierFormModel();
 
             return View(model);
         }
+
         [HttpPost]
+        [NotAnRentier]
         public async Task<IActionResult> Become(BecomeRentierFormModel model)
         {
+            if (await rentierService.UserWithPhoneNumberExistsAsync(model.PhoneNumber))
+            {
+                ModelState.AddModelError(nameof(model.PhoneNumber), PhoneExists);
+            }
+
+            if (await rentierService.UserHasRentsAsync(User.Id()))
+            {
+                ModelState.AddModelError("Error", HasRents);
+            }
+
+            if (ModelState.IsValid == false)
+            {
+                return View(model);
+            }
+
+            await rentierService.CreateAsync(User.Id(), model.PhoneNumber);
+
             return RedirectToAction(nameof(CarController.All), "Car");
         }
     }
