cb1fb66b1e1db7d66506a60dd905ad2df82f6d49
Daniel Nikolov <126501@students.ue-varna.bg>
2024-03-31 17:59:05 +0300

    WebApi and Details security added

---
+++ RentOut.Core/Extensions/ModelExtensions.cs
@@ -0,0 +1,24 @@
+﻿using RentOut.Core.Contracts;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace RentOut.Core.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static string GetInformation(this ICarModel car)
+        {
+            string info = car.Title.Replace(" ", "-") + GetTown(car.Town);
+            info = Regex.Replace(info, @"[^a-zA-Z0-9\-]", string.Empty);
+
+            return info;
+        }
+
+        private static string GetTown(string town)
+        {
+            town = string.Join("-", town.Split(" ").Take(3));
+
+            return town;
+        }
+    }
+}
--- RentOut.Core/Models/Home/CarIndexServiceModel.cs
+++ RentOut.Core/Models/Home/CarIndexServiceModel.cs
@@ -1,11 +1,15 @@
-﻿namespace RentOut.Core.Models.Home
+﻿using RentOut.Core.Contracts;
+
+namespace RentOut.Core.Models.Home
 {
-    public class CarIndexServiceModel
+    public class CarIndexServiceModel : ICarModel
     {
         public int Id { get; set; }
 
         public string Title { get; set; } = string.Empty;
 
         public string ImageUrl { get; set; } = string.Empty;
+
+        public string Town { get; set; } = string.Empty;
     }
 }
